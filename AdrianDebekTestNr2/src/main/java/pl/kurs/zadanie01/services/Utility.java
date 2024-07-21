5c6b3db49f1eb9b4208888cba6ffc6508fbba30b
Lachuder <164665686+Lachuder@users.noreply.github.com>
2024-07-21 17:29:11 +0200

    Delete AdrianDebekTestNr2 directory

--- AdrianDebekTestNr2/src/main/java/pl/kurs/zadanie01/services/Utility.java
+++
@@ -1,9 +0,0 @@
-package pl.kurs.zadanie01.services;
-
-public interface Utility {
-
-    static int convertStringToInt(String input) {
-        return Integer.parseInt(input);
-    }
-
-}
--- AdrianDebekTestNr2/src/main/java/pl/kurs/zadanie04/Runner.java
+++
@@ -1,7 +0,0 @@
-package pl.kurs.zadanie04;
-
-public class Runner {
-    public static void main(String[] args) {
-
-    }
-}
